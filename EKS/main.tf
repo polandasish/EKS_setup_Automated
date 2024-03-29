735a6bdbc3d1627efe20cb22ce62e5934daed726
polandasish <139715583+polandasish@users.noreply.github.com>
2024-03-29 08:20:56 +0100

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -71,7 +71,7 @@ module "eks" {
     Name= "eks"
   }
 }
-
+/*
 data "aws_eks_cluster" "cluster" {
   name= module.eks.cluster_id
   
@@ -80,4 +80,5 @@ data "aws_eks_cluster_auth" "cluster" {
   name= module.eks.cluster_id
   
 }
+*/
 
