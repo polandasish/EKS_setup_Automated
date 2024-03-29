4e324e8f45a398b5c67aba243309511df67c5e92
polandasish <139715583+polandasish@users.noreply.github.com>
2024-03-29 07:47:40 +0100

    Update output.tf

--- EKS/output.tf
+++ EKS/output.tf
@@ -1,8 +1,11 @@
-
-
 output "cluster_id" {
-  description = "EKS cluster ID."
-  value       = module.eks.cluster_id
+  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
+  value       = try(aws_eks_cluster.this[0].cluster_id, null)
+}
+
+output "cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = try(aws_eks_cluster.this[0].name, null)
 }
 
 output "cluster_endpoint" {
@@ -20,8 +23,16 @@ output "region" {
   description = "AWS region"
   value       = var.region
 }
-
+/*
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
   value       = module.eks.cluster_name
 }
+
+output "cluster_id" {
+  description = "EKS cluster ID."
+  value       = module.eks.cluster_id
+}
+*/
+
+
