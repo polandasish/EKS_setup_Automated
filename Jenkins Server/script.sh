8aa5560beaf1e8b43b787962c7dbd7116f85d8f0
polandasish <139715583+polandasish@users.noreply.github.com>
2024-04-08 23:27:36 +0200

    Update script.sh

--- Jenkins Server/script.sh
+++ Jenkins Server/script.sh
@@ -65,3 +65,11 @@ sudo usermod -aG docker $USER
 newgrp docker
 docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
 sudo apt-get update -y
+
+sudo apt-get update -y
+sudo apt-get install wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+sudo apt-get install maven -y
