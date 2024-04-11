86f024a0996b60c5edecabdb1ae9c1694daedc49
polandasish <139715583+polandasish@users.noreply.github.com>
2024-04-11 20:19:03 +0200

    Update script.sh

--- Jenkins Server/script.sh
+++ Jenkins Server/script.sh
@@ -94,6 +94,7 @@ rm -rf node_exporter*
 sudo apt-get update
 sudo apt-get install -y apt-transport-https software-properties-common
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
 sudo apt-get -y install grafana
 sudo systemctl enable grafana-server
