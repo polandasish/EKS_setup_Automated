dfe786abf443f9ac0706d1b73bde8eb275071a59
polandasish <139715583+polandasish@users.noreply.github.com>
2024-04-13 08:00:51 +0200

    Update script.sh

--- Jenkins Server/script.sh
+++ Jenkins Server/script.sh
@@ -103,5 +103,5 @@ sudo systemctl start grafana-server
 #Helm Install
 curl -O https://get.helm.sh/helm-v3.12.3-linux-amd64.tar.gz
 tar -xvf helm-v3.12.3-linux-amd64.tar.gz
-sudo cp linux-amd64/helm /usrlocal/bin
+sudo cp linux-amd64/helm /usr/local/bin
 
